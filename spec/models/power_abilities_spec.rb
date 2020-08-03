19321b809fb1816985837b130737b48bfc48415d
Deonte Cooper <deonte.cooper12@gmail.com>
2020-08-02 21:44:56 -0600

    Add PowerAbilities model tesing

---
+++ spec/models/power_abilities_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe PowerAbilities, type: :model do
+  describe 'Validations' do
+    it { should validate_presence_of :power }
+    it { should validate_presence_of :color }
+    it { should validate_presence_of :ability }
+    it { should validate_presence_of :desciption }
+  end
+end
