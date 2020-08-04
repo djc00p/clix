7a8a7e166a1b69ef80db5cda0e6fba67d81784d3
Deonte Cooper <deonte.cooper12@gmail.com>
2020-08-03 21:07:54 -0600

    Add KeyphraseAbilities to schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_045545) do
+ActiveRecord::Schema.define(version: 2020_08_04_020602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "keyphrase_abilities", force: :cascade do |t|
+    t.string "keyphrase"
+    t.string "symbol"
+    t.text "description"
+  end
 
   create_table "power_abilities", force: :cascade do |t|
     t.string "power"
