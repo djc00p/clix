4c5576820e5a3b452c7afa926c8c4d433a9ca28e
Deonte Cooper <deonte.cooper12@gmail.com>
2020-08-03 21:04:42 -0600

    Add KeyphraseAbilities model tesing

---
+++ spec/models/keyphrase_abilities_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe KeyphraseAbilities, type: :model do
+  describe 'Validations' do
+    it { should validate_presence_of :keyphrase }
+    it { should validate_presence_of :description }
+  end
+end
