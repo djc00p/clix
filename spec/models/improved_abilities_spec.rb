cfffc51548be4d64b393b6a3c668861b1cc677ab
Deonte Cooper <deonte.cooper12@gmail.com>
2020-08-03 21:20:44 -0600

    Add ImprovedAbilities model testing

---
+++ spec/models/improved_abilities_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe ImprovedAbilities, type: :model do
+  describe 'Validations' do
+    it { should validate_presence_of :symbol }
+    it { should validate_presence_of :movement }
+    it { should validate_presence_of :targeting }
+  end
+end
