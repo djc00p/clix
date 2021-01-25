8c9c2582776b195c9f414cc7529909f2b4991097
Deonte Cooper <deonte.cooper12@gmail.com>
2021-01-25 09:40:08 -0700

    minor changes to intial files

--- app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
+++ app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
@@ -1,19 +1,20 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import PropTypes from "prop-types";
+import React from "react";
 
 const HelloWorld = ({ name, updateName }) => (
   <div>
-    <h3>
-      Hello,
-      {name}!
-    </h3>
-    <hr />
+    <h3> Hello, {name}! </h3> <hr />
     <form>
       <label htmlFor="name">
         Say hello to:
-        <input id="name" type="text" value={name} onChange={(e) => updateName(e.target.value)} />
-      </label>
-    </form>
+        <input
+          id="name"
+          type="text"
+          value={name}
+          onChange={(e) => updateName(e.target.value)}
+        />{" "}
+      </label>{" "}
+    </form>{" "}
   </div>
 );
 
