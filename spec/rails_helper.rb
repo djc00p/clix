efc2712ba85c1342ec136fb07d528ee8a27ee98e
Deonte Cooper <deonte.cooper12@gmail.com>
2021-02-11 08:54:52 -0700

    Add simplecov formatter for coveralls

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,7 @@ require 'rspec/rails'
 require 'capybara/rails'
 require 'support/factory_bot'
 require 'simplecov'
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 SimpleCov.start
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
