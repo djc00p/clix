6872170236a4e3dc05988b6c0a469ecdb90739ae
Deonte Cooper <deonte.cooper12@gmail.com>
2021-02-11 09:08:26 -0700

    Set up Capybara configuration for integration tests to support multiple drivers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'capybara/rails'
+require "capybara-screenshot/rspec"
+require "webdrivers"
 require 'support/factory_bot'
 require 'simplecov'
 SimpleCov.formatter = Coveralls::SimpleCov::Formatter
@@ -67,6 +69,60 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  # Capybara config
+  #
+  # selenium_firefox webdriver only works for Travis-CI builds.
+  default_driver = :selenium_chrome_headless
+
+  supported_drivers = %i[ selenium_chrome_headless selenium_chrome selenium_firefox selenium]
+  driver = ENV["DRIVER"].try(:to_sym) || default_driver
+  Capybara.default_driver = driver
+
+  raise "Unsupported driver: #{driver} (supported = #{supported_drivers})" unless supported_drivers.include?(driver)
+
+  case driver
+  when :selenium_chrome
+    DriverRegistration.register_selenium_chrome
+
+  when :selenium_chrome_headless
+    DriverRegistration.register_selenium_chrome_headless
+
+  when :selenium_firefox, :selenium
+    DriverRegistration.register_selenium_firefox
+    driver = :selenium_firefox
+  end
+
+  Capybara.javascript_driver = driver
+  Capybara.default_driver = driver
+
+  Capybara.register_server(Capybara.javascript_driver) do |app, port|
+    require "rack/handler/puma"
+    Rack::Handler::Puma.run(app, Port: port)
+  end
+  Capybara.server = :puma
+
+  config.before(:each, type: :system, js: true) do
+    driven_by driver
+    driven_by :selenium, using: :chrome, options: { args: ["headless", "disable-gpu", "no-sandbox", "disable-dev-shm-usage"] }
+  end
+
+  # Capybara.default_max_wait_time = 15
+  puts "=" * 80
+  puts "Capybara using driver: #{Capybara.javascript_driver}"
+  puts "=" * 80
+
+  Capybara.save_path = Rails.root.join("tmp", "capybara")
+  Capybara::Screenshot.prune_strategy = { keep: 10 }
+
+  config.append_after(:each) do
+    Capybara.reset_sessions!
+  end
+
+  # This will insert a <base> tag with the asset host into the pages created by
+  # save_and_open_page, meaning that relative links will be loaded from the
+  # development server if it is running.
+  Capybara.asset_host = "http://localhost:3000"
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
