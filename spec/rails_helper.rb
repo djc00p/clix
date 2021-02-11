9fe44a2cd622b435befd91e01408cf66c5cb3e50
Deonte Cooper <deonte.cooper12@gmail.com>
2021-02-11 08:57:27 -0700

    Add NODE_ENV to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
+ENV['RAILS_ENV'] ||= "test"
+ENV["NODE_ENV"] ||= "test"
 require "coveralls"
 Coveralls.wear!("rails") # must occur before any of your application code is required
 require File.expand_path('../config/environment', __dir__)
