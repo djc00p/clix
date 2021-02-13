95180cac7f458aa1593a8d97d4aca0decafbd45e
Deonte Cooper <deonte.cooper12@gmail.com>
2021-02-12 23:36:56 -0700

    Add register_selenium_firefox method to DriverRegistration module

--- spec/support/driver_registration.rb
+++ spec/support/driver_registration.rb
@@ -9,4 +9,15 @@ module DriverRegistration
     end
     @selenium_chrome_registered = true
   end
+
+  def self.register_selenium_firefox
+    return if @selenium_firefox_registered
+    Capybara.register_driver :selenium_firefox do |app|
+      Capybara::Selenium::Driver.new(app, browser: :firefox)
+    end
+    Capybara::Screenshot.register_driver(:selenium_firefox) do |js_driver, path|
+      js_driver.browser.save_screenshot(path)
+    end
+    @selenium_firefox_registered = true
+  end
 end
