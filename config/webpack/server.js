2832096d3e5ee6d399f998bf87b4e50b03099dff
Deonte Cooper <deonte.cooper12@gmail.com>
2021-02-15 12:30:39 -0700

    the merge function moved to a named export instead of being the default export

--- config/webpack/server.js
+++ config/webpack/server.js
@@ -1,5 +1,5 @@
 const environment = require("./environment");
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const devBuild =
   process.env.NODE_ENV === "production" ? "production" : "development";
 const webpack = require("webpack");
